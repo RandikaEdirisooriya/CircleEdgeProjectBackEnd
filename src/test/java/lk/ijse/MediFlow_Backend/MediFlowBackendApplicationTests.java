956b9645367e16f4664a32a0721ac141d0623cdc
RandikaEdirisooriya <randikaedirisooriya@gmail.com>
2024-06-26 11:48:43 +0530

    CircleEdgeProjectBackEnd

---
+++ src/test/java/lk/ijse/MediFlow_Backend/MediFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.MediFlow_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediFlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
